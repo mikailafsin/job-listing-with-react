3199e43407cfba47bb51d389e5550e21158dce82
mikailafsin <mikailwnchstr@gmail.com>
2024-05-31 20:16:33 +0300

    PUT request to Update job

--- src/pages/EditJobPage.jsx
+++ src/pages/EditJobPage.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate, useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 
-const EditJobPage = () => {
+const EditJobPage = ({ updateJobSubmit }) => {
     const job = useLoaderData();
 
     const [title, setTitle] = useState(job.title);
@@ -14,14 +15,37 @@ const EditJobPage = () => {
     const [contactEmail, setContactEmail] = useState(job.company.contactEmail);
     const [contactPhone, setContactPhone] = useState(job.company.contactPhone);
 
-    const submitForm = (e) => {};
+    const navigate = useNavigate();
+    const { id } = useParams();
+
+    const submitForm = (e) => {
+        e.preventDefault();
+        const updatedJob = {
+            id,
+            title,
+            type,
+            location,
+            description,
+            salary,
+            company: {
+                name: companyName,
+                description: companyDescription,
+                contactEmail,
+                contactPhone,
+            },
+        };
+        updateJobSubmit(updatedJob);
+        toast.success("Job updated successfully.");
+
+        return navigate(`/jobs/${id}`);
+    };
 
     return (
         <section className="bg-indigo-50">
             <div className="container m-auto max-w-2xl py-24">
                 <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
                     <form onSubmit={submitForm}>
-                        <h2 className="text-3xl text-center font-semibold mb-6">Add Job</h2>
+                        <h2 className="text-3xl text-center font-semibold mb-6">Update Job</h2>
 
                         <div className="mb-4">
                             <label htmlFor="type" className="block text-gray-700 font-bold mb-2">
@@ -177,7 +201,7 @@ const EditJobPage = () => {
                                 className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
                                 type="submit"
                             >
-                                Add Job
+                                Update Job
                             </button>
                         </div>
                     </form>
