06d977830da42e8f0e7f5aba438d27c6aa726988
mikailafsin <mikailwnchstr@gmail.com>
2024-05-31 19:58:40 +0300

    edit job page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import JobPage, { jobLoader } from "./pages/JobPage";
 import AddJobPage from "./pages/AddJobPage";
+import EditJobPage from "./pages/EditJobPage";
 
 const App = () => {
     // Add New Job
@@ -34,6 +35,7 @@ const App = () => {
                 <Route index element={<HomePage />} />
                 <Route path="/jobs" element={<JobsPage />} />
                 <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
+                <Route path="/edit-job/:id" element={<EditJobPage />} loader={jobLoader} />
                 <Route path="/jobs/:id" element={<JobPage deleteJob={deleteJob} />} loader={jobLoader} />
                 <Route path="*" element={<NotFoundPage />} />
             </Route>
